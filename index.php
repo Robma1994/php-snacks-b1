dbe9c66e8ac00475845d2cd1416ed70d8414e768
Roberto <robertomartinonwg@gmail.com>
2021-10-08 18:06:04 +0200

    Bonus snack 1 - numeri randomici

--- index.php
+++ index.php
@@ -59,14 +59,47 @@
             che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e 
             che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
             */
-            if( empty($mail) && empty($name) && empty($age)) {
-                echo 'Inserisci Nome - Mail - Age' ;
+            if( empty($name) && empty($mail) && empty($age)) {
+                echo 'Inserisci Nome(almeno 3 caratteri) - Mail(ricorda @ e .) - Age(inserisci un carattere numerico)' ;
             } 
+            
             elseif((strlen($name) > 3) && (strpos($mail, '@')) && (strpos($mail, '.')) && (is_numeric($age))) {
                 echo 'Accesso riuscito';
             } 
             else {
-                echo 'Accesso negato';
+                echo 'Accesso negato' ;
+            }
+        ?>
+    </div>
+    <div class="snack1-bonus">
+        <?php
+            //A) Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario.
+            //Ricordati che ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite in maniera randomica.
+            $teams = 
+            [
+                [
+                    'homeTeam' => 'Boston Celtics',
+                    'visitingTeam' => 'Virtus',
+                    'pointsMade' => rand(1,100),
+                    'pointsSuffered' => rand(1,100),
+                ],
+                [
+                    'homeTeam' => 'Arsenal',
+                    'visitingTeam' => 'Varese',
+                    'pointsMade' => rand(1,100),
+                    'pointsSuffered' => rand(1,100),
+                ],
+                [
+                    'homeTeam' => 'Olimpia',
+                    'visitingTeam' => 'Chicago Bulls',
+                    'pointsMade' => rand(1,100),
+                    'pointsSuffered' => rand(1,100),
+                ]
+            ];
+            $rdnNumberPoints = rand(min, max);
+            //Stampiamo a schermo tutte le partite con questo schema. Olimpia Milano - Cantù | 55-60
+            for ($i = 0; $i < count($teams); $i++) {
+                echo $teams[$i]['homeTeam'] . ' ' . '-' . ' ' . $teams[$i]['visitingTeam'] . ' | ' . $teams[$i]['pointsMade'] . ' - ' . $teams[$i]['pointsSuffered'] . '<br/>';
             }
         ?>
     </div>
